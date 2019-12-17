0e4537f227ac5b002ca83afb706fc37150e984d7
Georgia Kirkpatrick <georgia.kirkpatrick@gmail.com>
2019-12-16 17:11:14 -0800

    Add mobile functionality to JS, CSS
    Add mobile features to css file so app works on phones.

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
     // specified by playlistID.
     function vidPlayer(vidID) {
         $('#video').html(`
-            <iframe width="560" height="315" 
+            <iframe class="main-vid" width="560" height="315" 
             src="https://www.youtube.com/embed/${vidID}" frameborder="0" 
             allow="accelerometer; autoplay; encrypted-media; gyroscope; 
             picture-in-picture" allowfullscreen></iframe>`
